0bfe44ff17c38cef3ba69ef1be02623530b63a7a
valeria-jefimova <108732968+valeria-jefimova@users.noreply.github.com>
2023-02-19 23:17:07 +0200

    updated def foo

--- str_func.py
+++ str_func.py
@@ -1,9 +1,2 @@
 def foo(s: str):
-    return s.capitalize()
-
-
-def foo2(s: str):
-    """
-    Функция, которая делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
-    """
-    return s.title()
+    return s.upper()
